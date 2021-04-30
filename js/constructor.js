e3aec64a9c6e603252216b09fc78cde10a38ca6c
Akyl <akylbekmuratbekuulu@gmail.com>
2021-04-30 22:50:14 +0600

    work with post

---
+++ js/constructor.js
@@ -0,0 +1,48 @@
+
+
+
+// function User(name, id){
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`)
+//     }
+// }
+
+// User.prototype.exit = function(){
+//     console.log(`User ${this.name} go`)
+// }
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('alex', 30);
+
+// ivan.exit()
+
+// ivan.hello();
+// alex.hello();
+// console.log(ivan)
+// console.log(alex)
+
+//  rest 
+
+
+// const log = function (a,b, ...rest){
+//     console.log(a,b,rest)
+// }
+
+// log('basic', 'rest', 'operator', 'usegae')
+
+// function calcOrDouble(number, basis = 2){
+   
+//     console.log(number + basis)
+// }
+// calcOrDouble(3);
+
+
+// ------= Json -----
+
+// const persone = {
+//     name: 'Alex',
+//     tel:'+0555337599'
+// };
--- js/script.js
+++ js/script.js
@@ -147,6 +147,129 @@ window.addEventListener('DOMContentLoaded', () =>{
   }
    }
    
-   window.addEventListener('scroll', showModalByScroll)
+   window.addEventListener('scroll', showModalByScroll);
+
+ // использую  классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes || 'menu__item';
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH(){
+           this.price = this.price * this.transfer;
+        }
+
+        render(){
+            const element = document.createElement('div');
+
+            if(this.classes.length === 0){
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            }else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                                
+                                    <img src=${this.src} alt=${this.alt}>
+                                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                                    <div class="menu__item-descr">${this.descr}</div>
+                                    <div class="menu__item-divider"></div>
+                                    <div class="menu__item-price">
+                                        <div class="menu__item-cost">Цена:</div>
+                                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                                    </div>
+                               
+                                `;
+                                this.parent.append(element);
+        }
+
+
+    }
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+        
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан! sfdddfshsdfhgfhjfjgk',
+        15,
+        '.menu .container'
+      
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        10,
+        '.menu .container'
+        
+    ).render();
+
+    // Forms 
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Thanks we will call you',
+        failure: 'something goes wrong'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form){
+        form.addEventListener('submit', (e) => {
+             e.preventDefault();
+
+             const statusMessage = document.createElement('div');
+             statusMessage.classList.add('status');
+             statusMessage.textContent = message.loading;
+             form.append(statusMessage);
+
+             const request = new XMLHttpRequest();
+             request.open('POST', 'server.php');
+
+
+             request.setRequestHeader('Content-type', 'multipart/form-data');
+             const formData = new FormData(form);
+
+             request.send(formData);
+
+
+             request.addEventListener('load', () => {
+                 if (request.status === 200){
+                     console.log(request.response);
+                     statusMessage.textContent = message.success;
+                 }else{
+                    statusMessage.textContent = message.failure;
+                 }
+             })
+        })
+    }
+     
 
 });
